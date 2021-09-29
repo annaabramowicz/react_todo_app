8f4cdb29ad6d16e8e5b2469c1057258df56071ae
anna abramowicz <annaabra1@gmail.com>
2021-09-29 14:14:40 +0200

    reorganized and first configuration step

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,71 +1,15 @@
-import "../reset.css";
+import "reset.css";
 import React from "react";
-import ToDoInput from "./ToDoInput/ToDoInput";
-import AddButton from "./AddButton/AddButton";
-import ToDosList from "./ToDoList/ToDosList";
-import ClearAllButton from "./ClearAllButton/ClearAllButton";
-import CurrentNumberOfTodos from "./CurrentNumberOfTodos/CurrentNumberOfTodos";
-import ToggleButton from "./ToggleBotton/ToggleBotton";
-import Flex from "../components/Flex/Flex";
-import Heading from "../components/Heading/Heading";
-import Box from "../components/Box/Box";
-import { useState } from "react";
-import { addTodo, removeAllTodos, removeTodo } from "store/todos/todos";
+import Button from "components/Buttons/Button/Button";
 import { useDispatch } from "react-redux";
-import { useColorModeValue } from "@chakra-ui/react";
-
-const getNewId = () => `${new Date().getTime()}`;
 
 function App() {
-  const [currentInputValue, setCurrentInputValue] = useState("");
   const dispatch = useDispatch();
-  const isButtonDisabled = !currentInputValue.trim();
-  const formBackground = useColorModeValue("gray.200", "gray.600");
-
-  const removeOneTodo = (todoId) => dispatch(removeTodo(todoId));
-  const removeAllTodo = () => dispatch(removeAllTodos());
-
-  const onAddButtonClick = () => {
-    const newTodo = {
-      id: getNewId(),
-      text: currentInputValue,
-    };
-    dispatch(addTodo(newTodo));
-    setCurrentInputValue("");
+  const handleClick = () => {
+    console.log("click works");
   };
 
-
-  return (
-    <>
-      <Box margin="50px auto" w="500px">
-        <Flex
-          boxShadow="lg"
-          borderRadius={10}
-          direction="column"
-          background={formBackground}
-          height="auto"
-        >
-          <Heading>React To Do App</Heading>
-          <Flex w="300px" p={5}>
-            <ToDoInput
-              setValue={setCurrentInputValue}
-              currentInputValue={currentInputValue}
-            />
-            <AddButton
-              onButtonClick={onAddButtonClick}
-              isDisabled={isButtonDisabled}
-            />
-          </Flex>
-        </Flex>
-        <Flex mb={5}>
-          <CurrentNumberOfTodos />
-          <ClearAllButton removeAll={removeAllTodo} />
-        </Flex>
-        <ToDosList removeTodo={removeOneTodo} />
-        <ToggleButton>Toggle Mode</ToggleButton>
-      </Box>
-    </>
-  );
+  return <Button onClick={handleClick}>Thunk example</Button>;
 }
 
 export default App;
--- src/components/Buttons/Button/Button.jsx
+++ src/components/Buttons/Button/Button.jsx
@@ -1,18 +1,7 @@
 import { Button as ChakraButton } from "@chakra-ui/button";
 
-const Button = (props) => {
-  const getColorScheme = () => {
-    if (props.type === "positive") {
-      return "black";
-    } else if (props.type === "negative") {
-      return "teal";
-    }
-    return "orange";
-  };
-
-  return (
-    <ChakraButton colorScheme={getColorScheme()} ml={2} variant="solid  " {...props} />
-  );
-};
+const Button = (props) => (
+  <ChakraButton colorScheme="teal" ml={2} variant="solid" {...props} />
+);
 
 export default Button;
